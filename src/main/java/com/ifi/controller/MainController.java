bea369b10b201a130cde088c25aad8f0edfb3ebe
said05 <thsaidbarry@yahoo.fr>
2013-12-09 13:23:53 +0100

    Mise en place des entites et des contolleurs principale et articleController

---
+++ src/main/java/com/ifi/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.ifi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+	public @ResponseBody String index()
+	{
+		return "index";
+	}
+}
