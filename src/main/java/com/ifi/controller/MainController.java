a6dfdeac51307958091d4cfaa7f948d06aed7189
said05 <thsaidbarry@yahoo.fr>
2013-12-09 18:47:07 +0100

    Rajout de la page index du blog avec les liens "ajouter article" et "acceuil". création du controller de gestion des commentaires.

--- src/main/java/com/ifi/controller/MainController.java
+++ src/main/java/com/ifi/controller/MainController.java
@@ -1,14 +1,28 @@
 package com.ifi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ifi.model.Article;
+import com.ifi.repositories.ArticleRepository;
 
 @Controller
 public class MainController {
-    @RequestMapping("/")
-	public @ResponseBody String index()
-	{
-		return "acceuil";
+    
+    @Autowired
+    ArticleRepository articleRepository;
+    @RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(Model model)
+	{  
+	   // on recuppère la liste de tous les articles publié afin de les afficher
+	     List<Article> articles=articleRepository.findAll();
+	     // on ajoute la liste des aticles à la vue
+	     model.addAttribute("articles",articles);
+		return "index";
 	}
 }
